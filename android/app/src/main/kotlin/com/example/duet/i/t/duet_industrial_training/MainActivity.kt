a541eb7eca73d03f325530f4bd151dc6aded7a22
abuubaida921 <ubaidanazia@gmail.com>
2023-12-26 18:55:48 +0600

    First Commit

---
+++ android/app/src/main/kotlin/com/example/duet/i/t/duet_industrial_training/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.duet.i.t.duet_industrial_training
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/second_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Congrats'),),
+      body: Center(child: Text('you are in second page')),
+    );
+  }
+}

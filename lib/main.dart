0ab6d14a76ccb88f1fa1ba6acc65f82e99583076
abuubaida921 <ubaidanazia@gmail.com>
2024-01-04 18:34:59 +0600

    Fetching data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:duet_industrial_training/second_screen.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -56,23 +56,37 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  // final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<dynamic> s_response = [];
+
+  Future<void> fetchUsers() async {
+    try {
+      final response = await http.get(Uri.parse('https://reqres.in/api/users?page=1'));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        print(jsonData);
+        setState(() {
+          s_response = jsonData['data'];
+        });
+      } else {
+        throw Exception('Failed to load users');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchUsers();
+  // }
 
   void _incrementCounter() {
     setState(() {
@@ -88,59 +102,98 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Center(
             child: Text('THis is a title'),
           )),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                child: Image.network(
-                    'https://avatars.githubusercontent.com/u/145796865?v=4'),
-              ),
-              Expanded(
-                child: Text(
-                  'Nur Nobi',
-                  style: TextStyle(fontSize: 30),
-                ).animate().fadeIn().scale().move(delay: 1000.ms,duration: 60.ms).blurXY(),
-              ),
-            ],
-          ),
-          Container(
-            color: Colors.red,
-            height: 300,
-            child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap:() {
-                      // Navigator.push(context, MaterialPageRoute(builder: (context)=> SecondScreen()));
-                      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Clicked on position no ${index + 1}')));
-                      Fluttertoast.showToast(msg: 'Clicked on position no');
-                    },
-                    title: Text('${index + 1}. Texon Limited',style: TextStyle(color: Colors.white),),
-                  );
-                }),
-          )
-        ],
+      
+      // body: Center(child: Text(s_response.toString()),),
+
+      body: ListView.builder(
+        itemCount: s_response.length,
+        itemBuilder: (context, index) {
+          final user = s_response[index];
+          return ListTile(
+            leading: CircleAvatar(backgroundImage: NetworkImage(user['avatar']),),
+            title: Text('${user['first_name']} ${user['last_name']}'),
+            subtitle: Text(user['email']),
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () { fetchUsers(); },child: Icon(Icons.download),),
+      // body: Center(
+      //     child: GestureDetector(
+      //       onTap: (){
+      //         fetchUsers();
+      //       },
+      //       child: Column(children: [
+      //         Text(
+      //           'FetchData from server',
+      //           style: TextStyle(fontSize: 20),
+      //         ),
+      //         SizedBox(
+      //           height: 40,
+      //         ),
+      //         s_response.isEmpty
+      //             ? Center(child: CircularProgressIndicator())
+      //             : ListView.builder(
+      //           itemCount: s_response.length,
+      //           itemBuilder: (context, index) {
+      //             final user = s_response[index];
+      //             return ListTile(
+      //               leading: CircleAvatar(
+      //                 backgroundImage: NetworkImage(user['avatar']),
+      //               ),
+      //               title: Text('${user['first_name']} ${user['last_name']}'),
+      //               subtitle: Text(user['email']),
+      //             );
+      //           },
+      //         ),
+      //       ],),
+      //     )),
+
+
+      // body: Column(
+      //   children: [
+      //     Row(
+      //       children: [
+      //         Container(
+      //           height: 100,
+      //           width: 100,
+      //           child: Image.network(
+      //               'https://avatars.githubusercontent.com/u/145796865?v=4'),
+      //         ),
+      //         Expanded(
+      //           child: Text(
+      //             'Nur Nobi',
+      //             style: TextStyle(fontSize: 30),
+      //           ).animate().fadeIn().scale().move(delay: 1000.ms,duration: 60.ms).blurXY(),
+      //         ),
+      //       ],
+      //     ),
+      //     Container(
+      //       color: Colors.red,
+      //       height: 300,
+      //       child: ListView.builder(
+      //           itemCount: 5,
+      //           itemBuilder: (context, index) {
+      //             return ListTile(
+      //               onTap:() {
+      //                 Navigator.push(context, MaterialPageRoute(builder: (context)=> SecondScreen()));
+      //                 // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Clicked on position no ${index + 1}')));
+      //                 // Fluttertoast.showToast(msg: 'Clicked on position no');
+      //               },
+      //               title: Text('${index + 1}. Texon Limited',style: TextStyle(color: Colors.white),),
+      //             );
+      //           }),
+      //     )
+      //   ],
+      // ),
       // body: Center(
       //   // Center is a layout widget. It takes a single child and positions it
       //   // in the middle of the parent.
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SecondScreen extends StatefulWidget {
   const SecondScreen({super.key});
@@ -10,9 +11,22 @@ class SecondScreen extends StatefulWidget {
 
 class _SecondScreenState extends State<SecondScreen> {
   @override
+  void initState() async {
+    super.initState();
+    final response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      throw Exception('Unable to fetch products from the REST API');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Congrats'),),
+      appBar: AppBar(
+        title: Text('Congrats'),
+      ),
       body: Center(child: Text('you are in second page')),
     );
   }
